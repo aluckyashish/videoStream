06dcfb0411d6c12664f0d148b054828aac47d6ad
Ashish Pandey <aluckyashish@gmail.com>
2021-07-22 14:30:03 +0530

    AWS updated

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
-var session = require('express-session')
+var session = require('express-session');
+// import entire SDK
+var AWS = require('aws-sdk');
+// import AWS object without services
+var AWS = require('aws-sdk/global');
+// import individual service
+var S3 = require('aws-sdk/clients/s3');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 //app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
@@ -23,14 +29,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'hcldemo'
 }))
+
+AWS.config.update(
+  {
+    "accessKeyId": "TODO",
+    "secretAccessKey": "TODO",
+    "region": "TODO"
+  }
+);
+const s3 = new AWS.S3();
+AWS.config.getCredentials(function (err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", AWS.config.credentials.accessKeyId);
+
+    var params = { Bucket: 'bucket', Key: AWS.config.credentials.accessKeyId };
+    var url = s3.getSignedUrl('getObject', params);
+    console.log('The URL is', url);
+  }
+});
 app.use('/', indexRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404,'File Not found'));
+app.use(function (req, res, next) {
+  next(createError(404, 'File Not found'));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
